8d22a41983ee532305d6c74d8cf81c87b141122d
Natalie Smith <thatgalnatalie@gmail.com>
2020-09-18 12:48:21 -0700

    updated tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,27 @@
+// module.exports = {
+//   future: {
+//     // removeDeprecatedGapUtilities: true,
+//     // purgeLayersByDefault: true,
+//   },
+//   purge: ['./pages/**/*.js', './src/components/**/*.js'],
+//   theme: {
+//     extend: {},
+//   },
+//   variants: {},
+//   plugins: [],
+// };
+
 module.exports = {
   future: {
-    // removeDeprecatedGapUtilities: true,
-    // purgeLayersByDefault: true,
+    removeDeprecatedGapUtilities: true,
   },
-  purge: ['./pages/**/*.js', './src/components/**/*.js'],
+  purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'accent-1': '#333',
+      },
+    },
   },
   variants: {},
   plugins: [],
